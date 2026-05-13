9ac863ac8786873640ea4eb7faca054d2020e27e
kalumemmanueljohn-blip <kalumemmanueljohn@gmail.com>
2026-05-13 18:02:38 +0100

    Update urls.py

--- events/urls.py
+++ events/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.events_list, name='events_list'),
+    path('add/', views.add_event, name='add_event'),
     path('<int:id>/', views.event_detail, name='event_detail'),
     path('<int:id>/register/', views.register_event, name='register_event'),
     path('<int:id>/delete/', views.delete_event, name='delete_event'),
-    path('add/', views.add_event, name='add_event'),  # ← Ajouter cette ligne
-    path('<int:id>/register/', views.register_event, name='register_event'),
 ]
